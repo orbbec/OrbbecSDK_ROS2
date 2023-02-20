28724885655a5064589ecdaa04680dd564e17987
Joe Dong <jian.dong@pobox.com>
2023-02-20 14:16:24 +0800

    clean .make_deb

--- orbbec_camera/.make_deb.sh
+++ orbbec_camera/.make_deb.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/bash
 set -e
 CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-rm -fr debian .obj-x86_64-linux-gnu
-bloom-generate rosdebian --os-name ubuntu --ros-distro $ROS_DISTRO  
-sed -i 's/$@/$@ --parallel/g' debian/rules
-PKG_NAME=$(cat package.xml| grep "<name>" | awk -F '>' '{print $2}' | awk -F '<'  '{print $1}')
-PKG_NAME=$(echo "ros-$ROS_DISTRO-${PKG_NAME}" | tr '_' '-')
-VERSION=$(cat package.xml| grep "<version>" | awk -F '>' '{print $2}' | awk -F '<'  '{print $1}')
-STAMP=$(date  +'%Y%m%d.%H%M%S')
-sed -i "1 s/.*/${PKG_NAME} (${VERSION}-1focal.${STAMP}) focal; urgency=high/g" debian/changelog
+rm -fr debian obj-x86_64-linux-gnu
+export DEBIAN_INC=1
+bloom-generate rosdebian --os-name ubuntu --ros-distro $ROS_DISTRO -i ${DEBIAN_INC}.$(date +%Y%m%d.%H%M%S)
 export DEB_BUILD_OPTIONS="parallel=$(($(nproc) - 1))"
 fakeroot debian/rules binary

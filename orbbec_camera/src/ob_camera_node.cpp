7e81252644a650f71d282aa45384e8a53a03bd22
Joe Dong <jian.dong@pobox.com>
2023-09-05 18:06:52 +0800

    remove unused tf

--- orbbec_camera/src/ob_camera_node.cpp
+++ orbbec_camera/src/ob_camera_node.cpp
@@ -1011,21 +1011,16 @@ void OBCameraNode::calcAndPublishStaticTransform() {
   rclcpp::Time tf_timestamp = node_->now();
 
   publishStaticTF(tf_timestamp, trans, Q, frame_id_[DEPTH], frame_id_[COLOR]);
-  publishStaticTF(tf_timestamp, zero_trans, quaternion_optical, frame_id_[COLOR],
-                  optical_frame_id_[COLOR]);
-  publishStaticTF(tf_timestamp, zero_trans, quaternion_optical, frame_id_[DEPTH],
-                  optical_frame_id_[DEPTH]);
-  publishStaticTF(tf_timestamp, zero_trans, quaternion_optical, frame_id_[ACCEL],
-                  optical_frame_id_[ACCEL]);
-  publishStaticTF(tf_timestamp, zero_trans, quaternion_optical, frame_id_[GYRO],
-                  optical_frame_id_[GYRO]);
-  publishStaticTF(tf_timestamp, zero_trans, quaternion_optical, frame_id_[INFRA0],
-                  optical_frame_id_[INFRA0]);
-  publishStaticTF(tf_timestamp, zero_trans, zero_rot, camera_link_frame_id_, frame_id_[DEPTH]);
-  publishStaticTF(tf_timestamp, zero_trans, zero_rot, camera_link_frame_id_, frame_id_[INFRA0]);
   publishStaticTF(tf_timestamp, trans, Q, camera_link_frame_id_, frame_id_[COLOR]);
-  publishStaticTF(tf_timestamp, zero_trans, zero_rot, camera_link_frame_id_, frame_id_[ACCEL]);
-  publishStaticTF(tf_timestamp, zero_trans, zero_rot, camera_link_frame_id_, frame_id_[GYRO]);
+  for (const auto &stream_index : IMAGE_STREAMS) {
+    if (stream_index == COLOR || !enable_stream_[stream_index]) {
+      continue;
+    }
+    publishStaticTF(tf_timestamp, zero_trans, zero_rot, camera_link_frame_id_,
+                    frame_id_[stream_index]);
+    publishStaticTF(tf_timestamp, zero_trans, quaternion_optical, frame_id_[stream_index],
+                    optical_frame_id_[stream_index]);
+  }
 }
 
 void OBCameraNode::publishStaticTransforms() {

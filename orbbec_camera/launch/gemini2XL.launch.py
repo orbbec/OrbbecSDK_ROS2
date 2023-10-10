c1d01765a50851333aca19840910a3810033cf3e
lixiaobin <lixiaobin@orbbec.com>
2023-10-10 09:31:44 +0800

    Modify multi device sync config parameter.

--- orbbec_camera/launch/gemini2XL.launch.py
+++ orbbec_camera/launch/gemini2XL.launch.py
@@ -79,8 +79,8 @@ def generate_launch_description():
         DeclareLaunchArgument('depth_delay_us', default_value='0'),
         DeclareLaunchArgument('color_delay_us', default_value='0'),
         DeclareLaunchArgument('trigger2image_delay_us', default_value='0'),
-        DeclareLaunchArgument('trigger_output_delay_us', default_value='0'),
-        DeclareLaunchArgument('trigger_output_enabled', default_value='false'),
+        DeclareLaunchArgument('trigger_out_delay_us', default_value='0'),
+        DeclareLaunchArgument('trigger_out_enabled', default_value='false'),
     ]
 
     # Node configuration

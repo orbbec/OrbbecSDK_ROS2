81b60a3abb06ff1e1942a6a87ee3a344b9614b18
Joe Dong <jian.dong@pobox.com>
2024-02-29 09:24:12 +0800

    Enable depth scale in OBCameraNode

--- orbbec_camera/launch/dabai_d1.launch.py
+++ orbbec_camera/launch/dabai_d1.launch.py
@@ -50,6 +50,7 @@ def generate_launch_description():
         DeclareLaunchArgument('soft_filter_speckle_size', default_value='-1'),
         DeclareLaunchArgument('ordered_pc', default_value='false'),
         DeclareLaunchArgument('use_hardware_time', default_value='false'),
+        DeclareLaunchArgument('enable_depth_scale', default_value='true'),
     ]
 
     # Node configuration
--- orbbec_camera/launch/femto_mega.launch.py
+++ orbbec_camera/launch/femto_mega.launch.py
@@ -79,6 +79,7 @@ def generate_launch_description():
         DeclareLaunchArgument('enable_frame_sync', default_value='true'),
         DeclareLaunchArgument('ordered_pc', default_value='false'),
         DeclareLaunchArgument('use_hardware_time', default_value='false'),
+        DeclareLaunchArgument('enable_depth_scale', default_value='true'),
     ]
 
     # Node configuration

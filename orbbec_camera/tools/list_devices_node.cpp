4704100c3c2cab90b9a46b4a2310fa9262f15abb
jj <957713278@qq.com>
2025-05-22 11:27:46 +0800

    Update README.MD

--- orbbec_camera/tools/list_devices_node.cpp
+++ orbbec_camera/tools/list_devices_node.cpp
@@ -20,25 +20,42 @@
 
 int main() {
   try {
+    ob::Context::setLoggerSeverity(OBLogSeverity::OB_LOG_SEVERITY_OFF);
     auto context = std::make_unique<ob::Context>();
-    context->setLoggerSeverity(OBLogSeverity::OB_LOG_SEVERITY_OFF);
     auto list = context->queryDeviceList();
     for (size_t i = 0; i < list->deviceCount(); i++) {
-      auto device = list->getDevice(i);
-      auto device_info = device->getDeviceInfo();
-      std::string serial = device_info->serialNumber();
-      std::string uid = device_info->uid();
-      auto usb_port = orbbec_camera::parseUsbPort(uid);
-      std::stringstream pid_hex;
-      pid_hex<< std::hex << std::setw(4) << std::setfill('0') << device_info->getPid();
-      RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"),
-                         "- Name: " << device_info->getName() << ", PID: 0x"
-                                    << pid_hex.str() << ", SN/ID: " << serial
-                                    << ", Connection: " << device_info->getConnectionType());
-      RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"), "serial: " << serial);
-      RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"), "usb port: " << usb_port);
-      RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"),
-                         "usb connect type: " << device_info->getConnectionType());
+      if (std::string(list->getConnectionType(i)) != "Ethernet") {
+        std::string serial = list->serialNumber(i);
+        std::string uid = list->uid(i);
+        auto usb_port = orbbec_camera::parseUsbPort(uid);
+        auto connection_type = list->getConnectionType(i);
+        std::stringstream pid_hex;
+        pid_hex << std::hex << std::setw(4) << std::setfill('0') << list->getPid(i);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"),
+                           "- Name: " << list->getName(i) << ", PID: 0x" << pid_hex.str()
+                                      << ", SN/ID: " << serial
+                                      << ", Connection: " << connection_type);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"), "serial: " << serial);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"), "usb port: " << usb_port);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"),
+                           "usb connect type: " << connection_type);
+        std::cout << std::endl;
+      } else {
+        std::string serial = list->serialNumber(i);
+        auto connection_type = list->getConnectionType(i);
+        auto ip_address = list->getIpAddress(i);
+        std::stringstream pid_hex;
+        pid_hex << std::hex << std::setw(4) << std::setfill('0') << list->getPid(i);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"),
+                           "- Name: " << list->getName(i) << ", PID: 0x" << pid_hex.str()
+                                      << ", SN/ID: " << serial
+                                      << ", Connection: " << connection_type);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"), "serial: " << serial);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"), "ip address: " << ip_address);
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("list_device_node"),
+                           "usb connect type: " << connection_type);
+        std::cout << std::endl;
+      }
     }
   } catch (ob::Error &e) {
     RCLCPP_ERROR_STREAM(rclcpp::get_logger("list_device_node"), e.getMessage());

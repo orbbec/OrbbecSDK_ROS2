54a5aedbbf2a821a428411f1dcc04c160a7f8cce
ob-yalian <luotao2025@163.com>
2025-11-11 10:43:07 +0800

    fix: update depth image transport plugins to remove unnecessary theora option

--- orbbec_camera/launch/gemini435_le.launch.py
+++ orbbec_camera/launch/gemini435_le.launch.py
@@ -259,7 +259,7 @@ def generate_launch_description():
         #color image transport plugins
         DeclareLaunchArgument('color.image_raw.enable_pub_plugins',default_value='["image_transport/compressed", "image_transport/raw", "image_transport/theora"]'),
         #depth image transport plugins
-        DeclareLaunchArgument('depth.image_raw.enable_pub_plugins',default_value='["image_transport/compressedDepth", "image_transport/raw", "image_transport/theora"]'),
+        DeclareLaunchArgument('depth.image_raw.enable_pub_plugins',default_value='["image_transport/compressedDepth", "image_transport/raw"]'),
         #infra1
         DeclareLaunchArgument('left_ir.image_raw.enable_pub_plugins',default_value='["image_transport/compressed", "image_transport/raw", "image_transport/theora"]'),
         #infra2

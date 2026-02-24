a7dd35de8a9171027589c78ec9ff126e51a7d9f2
Synapath_oscar_pc <happydrones123@gmail.com>
2026-02-24 14:52:28 +0800

    fix: change default value of enable_d2c_viewer to true in launch arguments

--- orbbec_camera/launch/gemini_330_series.launch.py
+++ orbbec_camera/launch/gemini_330_series.launch.py
@@ -193,7 +193,7 @@ def generate_launch_description():
         DeclareLaunchArgument('log_level', default_value='none'),
         DeclareLaunchArgument('log_file_name', default_value=''),
         DeclareLaunchArgument('enable_publish_extrinsic', default_value='false'),
-        DeclareLaunchArgument('enable_d2c_viewer', default_value='false'),
+        DeclareLaunchArgument('enable_d2c_viewer', default_value='true'),
         DeclareLaunchArgument('disparity_to_depth_mode', default_value='HW'),
         DeclareLaunchArgument('enable_ldp', default_value='true'),
         DeclareLaunchArgument('ldp_power_level', default_value='-1'),
